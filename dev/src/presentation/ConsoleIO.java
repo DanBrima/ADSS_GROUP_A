82d804713518c72389bee79f6302d2aaab77d740
דן ברימה <danbrima@macbook.local>
2024-09-09 23:28:58 +0300

    feat(entire project): add presentation layers

--- dev/src/presentation/ConsoleIO.java
+++ dev/src/presentation/ConsoleIO.java
@@ -1,4 +1,22 @@
 package presentation;
 
-public class ConsoleIO extends IO{
+import java.util.Scanner;
+
+public class ConsoleIO extends IO {
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    void print(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    String readString(String prompt) {
+        return scanner.nextLine();
+    }
+
+    @Override
+    int readInt(String prompt) {
+        return Integer.parseInt(scanner.nextLine());
+    }
 }
--- dev/src/presentation/IO.java
+++ dev/src/presentation/IO.java
@@ -1,4 +1,19 @@
 package presentation;
 
 abstract public class IO {
+    private static IO instance;
+
+    public static IO getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(IO io) {
+        instance = io;
+    }
+
+    abstract void print(String message);
+
+    abstract String readString(String prompt);
+
+    abstract int readInt(String prompt);
 }
