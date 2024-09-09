2c50ac29fb5354bc34ef242516beb00a050dc31d
sphereflowuser <sphereflow@gmail.com>
2024-09-09 22:11:42 +0300

    feat(): added Storage and itam stack objects

---
+++ dev/src/Domain/ItemStack.java
@@ -0,0 +1,33 @@
+package Domain;
+
+import java.util.ArrayList;
+
+public class ItemStack {
+
+    private Item itemType;
+
+    private ArrayList<ItemInstance> itemsList;
+
+    // Constructor for empty stack of certain type
+    public ItemStack(Item itemType) {
+        this.itemType = itemType;
+        this.itemsList = new ArrayList<ItemInstance>();
+    }
+
+    // Constructor for a new stack from one item
+    public ItemStack(ItemInstance item) {
+        this.itemType = item.getItemType();
+        this.itemsList = new ArrayList<ItemInstance>();
+        this.addItem(item);
+    }
+
+    // Add item if of the right typet
+    private void addItem(ItemInstance item) {
+        if (item.getItemType().getBARCODE() != this.itemType.getBARCODE()) {
+            throw new RuntimeException("The item " + item.getItemType() + " with barcode: " + item.getItemType().getBARCODE()
+                    + " doesn't fit the barcode of type " + this.itemType.getName() + " which is: " + this.itemType.getBARCODE());
+        } else {
+            this.itemsList.add(item);
+        }
+    }
+}
