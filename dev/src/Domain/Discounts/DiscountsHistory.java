c7a0b5bddf7263b4d4d1652cbadd35b4e9b2182a
sphereflowuser <sphereflow@gmail.com>
2024-09-11 20:44:00 +0300

    feat(): added discount history

---
+++ dev/src/Domain/Discounts/DiscountsHistory.java
@@ -0,0 +1,9 @@
+package Domain.Discounts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscountsHistory {
+    public static ArrayList<Discount> discountList = new ArrayList<>();
+
+}
