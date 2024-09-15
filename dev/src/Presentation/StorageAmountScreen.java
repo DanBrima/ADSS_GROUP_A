2aab355b5f2dd455ada590f9c2f1cc36a1cf5ae3
idan aviad <idanaviad10@gmail.com>
2024-09-15 22:54:02 +0300

    fix(): added more data about item when requesting for amount

--- dev/src/Presentation/StorageAmountScreen.java
+++ dev/src/Presentation/StorageAmountScreen.java
@@ -29,17 +29,18 @@ public class StorageAmountScreen extends Screen{
     }
 
     private void displayItemsFrom(ArrayList<ItemStack> itemStacks) {
-        String LEFT_ALIGN_FORMAT = "| %-11s | %-6s |%n";
+        String LEFT_ALIGN_FORMAT = "| %-11s | %-16s | %-11s | %-6s |%n";
 
         this.out.println();
-        this.out.format("+-------------+--------+%n");
-        this.out.format("| Item        | Amount |%n");
-        this.out.format("+-------------+--------+%n");
+        this.out.format("+-------------+------------------+-------------+--------+%n");
+        this.out.format("| Item        | Supplier         | Category    | Amount |%n");
+        this.out.format("+-------------+------------------+-------------+--------+%n");
         for (int itemStackIndex = 0; itemStackIndex < itemStacks.size(); itemStackIndex++) {
             ItemStack itemStack = itemStacks.get(itemStackIndex);
-            this.out.format(LEFT_ALIGN_FORMAT, itemStack.getItemType().getName(), itemStack.getItemCount());
+            this.out.format(LEFT_ALIGN_FORMAT, itemStack.getItemType().getName(), itemStack.getItemType().getSupplier(), itemStack.getItemType().getCategory().getName(), itemStack.getItemCount());
         }
-        this.out.format("+-------------+--------+%n");
+
+        this.out.format("+-------------+------------------+-------------+--------+%n");
         this.out.println();
     }
 }
